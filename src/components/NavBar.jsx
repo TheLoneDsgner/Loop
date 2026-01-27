7e39f89dfd9644c850e4e90d4b426430da22eb98
James David <james.designer19@gmail.com>
2026-01-27 18:07:26 +0100

    second commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,41 @@
 import myLogo from '../assets/images/Logo-loop.png'
 
 import { ArrowRight } from 'lucide-react';
+import { Camera } from 'lucide-react';
+import Button from './Button/Button';
 
 const NavBar = ({backButton, viewDashboard}) => {
     return ( 
         <nav className='navbar'>
             <div className="navbar-wrapper">
+
+                {/* logo */}
                 <div className="loop-logo">
                     {backButton}
+                                       
+                    {/* <div className="back-icon">{backButton}</div>                     */}
                     <img src={myLogo} alt="loop logo" />
                 </div>
+
+                
 
                 <div className="nav-links">                
                     <a href="#" className="nav-link_item">Features</a>
                     <a href="#" className="nav-link_item">How It Works</ a>                
                 </div>
 
-                {viewDashboard}
+                <div className="nav-left-items">
+                    {viewDashboard}
 
-                <div className="nav-btn-wrapper">
-                    {/* <Button text={'View recent lists'} iconRight={<ArrowRight   size=   {24} color='#4DB6AC'/>}/> */}
+                    <div className="nav-links_mobile">
+                        
+                    </div>
                 </div>
+                               
+                
             </div>
         </nav>
      );
 }
  
 export default NavBar;
-
-
-// import logo from '../assets/cowrywise-logo.png'
-// import search from '../assets/icons/search_line.svg'
-// import menu from '../assets/icons/menu_fill.svg'
-// import BtnPrimary from './BtnPrimary';
-// import { Link } from 'react-router-dom';
-// import BtnSecondary from './BtnSecondary';
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import NavBar from "../components/NavBar";
+import Button from "../components/Button/Button";
+import { ArrowRight } from "lucide-react";
+import { ChevronLeft } from "lucide-react";
 
 const Home = () => {
     return ( 
         <div className="home">
             <div className="hero">
-                <NavBar />
+                <NavBar
+                    // backButton={<ChevronLeft size={24}/>}
+                    
+                    viewDashboard={ <Button variant="secondary">View Recent List {<ArrowRight aria-label="arrow icon" size={20}/>}</Button>}
+                />
             </div>
         </div>
      );
